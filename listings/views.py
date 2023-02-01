cf1132db63edd70d03c52d45c7cba5c7ed00b28b
andyndem <112758283+andyndem@users.noreply.github.com>
2023-02-01 12:18:24 +0100

    imgs added

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from .models import Listing
 from .forms import ListingForm
-
 
 # Create your views here.
 # CRUD - create, retrieve, update, delete, list
@@ -20,6 +19,7 @@ def listing_retrieve(request, pk):
   }
   return render(request, "listings/listing.html", context)
 
+
 def listing_create(request):
   form = ListingForm()
   if request.method == "POST":
@@ -35,13 +35,12 @@ def listing_create(request):
   return render(request, "listings/listing-create.html", context)
 
 
-
 def listing_update(request, pk):
   listing = Listing.objects.get(id=pk)
   form = ListingForm(instance=listing)
   if request.method == "POST":
     listing = Listing.objects.get(id=pk)
-    form = ListingForm(request.POST, instance=listing)
+    form = ListingForm(request.POST, instance=listing, files=request.FILES)
     if form.is_valid():
       form.save()
       return HttpResponseRedirect("/")
@@ -50,37 +49,6 @@ def listing_update(request, pk):
     "form": form
   }
   return render(request, "listings/listing-update.html", context)
-
-
-
-
-# Bad code..form cannot prepopulate
-# # update view for details
-# def listing_update(request, pk):
-#   # Prepolulate the form
-#   listing = get_object_or_404(Listing, id = pk)
-#   form = ListingForm(instance=listing)
-#   # dictionary for initial data with field names as keys
-#   # context ={}
-# 	# fetch the object related to passed id
-#   listing = get_object_or_404(Listing, id = pk)
-# 	# pass the object as instance in form
-#   form = ListingForm(request.POST or None, instance = listing, files=request.FILES)
-# 	# save the data from the form and
-# 	# redirect to detail_view
-#   if form.is_valid():
-#     form.save()
-#     return HttpResponseRedirect("/")
-
-# 	# add form dictionary to context
-#   context = {
-#     "form": form
-#   }
-#   return render(request, "listings/listing-update.html", context)
-
-
-
-
 
 
 def listing_delete(request, pk):
